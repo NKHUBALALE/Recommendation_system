e95fdd62e1657c7e0323e5c80db88d9a705ee219
Nkhubalale <physimanuel@gmail.com>
2024-08-07 09:12:46 +0200

    about us

--- app.py
+++ app.py
@@ -340,7 +340,11 @@ def main():
             """
             This application was developed by Team JB3, a group of data science enthusiasts from the ExploreAI academy, as part of an unsupervised learning project. Our goal is to provide personalized anime recommendations based on user preferences and behaviors. By leveraging advanced recommendation algorithms and exploratory data analysis, we aim to enhance your anime discovery experience.
 
-This system uses collaborative filtering and content-based techniques to suggest anime that you might enjoy, helping you find new favorites based on your interests. For more information or inquiries, please contact us at jb3unsupervised@sandtech.co.za.
+This system uses collaborative filtering and content-based techniques to suggest anime that you might enjoy, helping you find new favorites based on your interests.
+
+We are always open to new project collaborations and would love to explore innovative ideas and partnerships, the world is in safe hands with us. For more information or inquiries, please contact us at your_email@sandtech.co.za.
+
+Thank you for your interest and support!
             """
         )
 
