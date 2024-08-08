64d429a81f5da3f769f9e87aab2a08365245669a
Nkhubalale <physimanuel@gmail.com>
2024-08-08 12:49:15 +0200

    email

--- app.py
+++ app.py
@@ -352,7 +352,7 @@ def main():
 
 This system uses collaborative filtering and content-based techniques to suggest anime that you might enjoy, helping you find new favorites based on your interests.
 
-We are always open to new project collaborations and would love to explore innovative ideas and partnerships, the world is in safe hands with us. For more information or inquiries, please contact us at your_email@sandtech.co.za.
+We are always open to new project collaborations and would love to explore innovative ideas and partnerships, the world is in safe hands with us. For more information or inquiries, please contact us at JB3unsupervised@sandtech.co.za.
 
 Thank you for your interest and support!
             """
